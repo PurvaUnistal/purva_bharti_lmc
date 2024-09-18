91f3ec85a97063497296b54f4afece2d40e2b94e
Unistal <spurva19@gmail.com>
2024-09-18 17:54:34 +0530

    removeStatusNGC

--- lib/service/Apis.dart
+++ lib/service/Apis.dart
@@ -10,7 +10,7 @@ class Apis {
   static String baseUatUrl = 'http://142.79.231.30:9097/api/';
 
    static String baseUrl = 'http://142.79.231.30:9097/api/';
-//  static String baseUrl = 'http://pbgpl.smartgasnet.com/api/';
+ // static String baseUrl = 'http://pbgpl.smartgasnet.com/api/';
   static String basePath = 'http://142.79.231.30:9097/';
 
   static String loginUrl = baseUrl + "auth";
